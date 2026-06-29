d6b1131446abc9e35487359199b9b2c0d9bec5e0
nanashi-1 <bagaresalken@gmail.com>
2026-06-29 13:17:36 +0800

    .

--- target/debug/deps/core_lib-790bc4838b5c87d8.d
+++ target/debug/deps/core_lib-790bc4838b5c87d8.d
@@ -1,9 +1,8 @@
-/home/nanashi/mnt/home_bkp/Documents/Rust/silkswitch/target/debug/deps/core_lib-790bc4838b5c87d8.d: core_lib/src/main.rs core_lib/src/../user1.dat.json Cargo.toml
+/home/nanashi/mnt/home_bkp/Documents/Rust/silkswitch/target/debug/deps/core_lib-790bc4838b5c87d8.d: core_lib/src/main.rs Cargo.toml
 
-/home/nanashi/mnt/home_bkp/Documents/Rust/silkswitch/target/debug/deps/libcore_lib-790bc4838b5c87d8.rmeta: core_lib/src/main.rs core_lib/src/../user1.dat.json Cargo.toml
+/home/nanashi/mnt/home_bkp/Documents/Rust/silkswitch/target/debug/deps/libcore_lib-790bc4838b5c87d8.rmeta: core_lib/src/main.rs Cargo.toml
 
 core_lib/src/main.rs:
-core_lib/src/../user1.dat.json:
 Cargo.toml:
 
 # env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
